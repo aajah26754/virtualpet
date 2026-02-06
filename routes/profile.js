c89d41540a50afd914a838e376380322ca8e3db1
benja26584 <benja26584@live.ytech.edu>
2026-02-06 09:58:56 -0500

    just some stuff

--- routes/profile.js
+++ routes/profile.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const isAuthenticated = require('../middleware/isAuthenticated');
+const inventory = require('modules/inventory.js');
 
 router.get('/', isAuthenticated, async (req, res) => {
-  const user = req.session.user;
+  const pets = await inventory.getInventoryForUser(req.session.user.username);
   
-  res.render('profile', {user});
+  res.render('profile', {
+    user: req.session.user,
+    pets
+  });
 });
 
 
